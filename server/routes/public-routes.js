dcc22f14386b3c99f2fed028fb429537296117c9
Justin Jackson <jsjbomo@gmail.com>
2018-07-23 16:41:21 -0700

    Adjust user pages to fit mobile screens

--- server/routes/public-routes.js
+++ server/routes/public-routes.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const {app} = require('./../app');
-const public = path.join(__dirname + '/../../public');
+const {app} = require('./../app')
+const public = path.join(__dirname + '/../../public')
+app.locals.public = public
 
 app.get('/img/:id', (req, res, next) => {
   let {id} = req.params;
-  if (id === 'paul.png') console.log('hello paul');
-  next();
+  if (id === 'paul.png') console.log('hello paul')
+  next()
 })
 
-app.use(express.static(public));
+app.use(express.static(public))
